39354971f59184fe53eda82a30e6d53423b71df1
thoibathoudam <thoiba@gmail.com>
2025-05-03 10:45:58 +0100

    fixed formtting

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
 # The configuration for the `remote` backend.
 terraform {
-    backend "remote" {
+  backend "remote" {
     # The name of your Terraform Cloud organization.
     organization = "tt-test-org"
 
     # The name of the Terraform Cloud workspace to store Terraform state files in.
     workspaces {
-        name = "test-workspace"
+      name = "test-workspace"
     }
-    }
+  }
 }
--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # An example resource that does nothing.
 resource "null_resource" "example" {
-    triggers = {
+  triggers = {
     value = "A example resource that does nothing!"
-    }
+  }
 }
